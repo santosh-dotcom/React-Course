8caa732076408d545996d81b77e5e75ef662c49c
santoshdeo <semtitans.santosh@gmail.com>
2023-08-10 10:11:28 +0530

    alert added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
 import "./App.css";
 import About from "./components/About";
+import Alert from "./components/Alert";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 import react, { useState } from 'react';
 
 function App() {
-  const[mode, setMode] = useState('light'); // whether dark mode is enabled or not.
+  const [mode, setMode] = useState('light'); // whether dark mode is enabled or not.
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type)=> {
+      setAlert({
+        msg: message,
+        type: type
+      })
+      setTimeout(() => {
+        setAlert(null);
+      }, 1500);
+  }
 
 const toggleMode = ()=>{
   if(mode === 'light'){
     setMode('dark');
     document.body.style.backgroundColor = '#042743';
+    showAlert("Dark mode has been enabled", "success");
   }
   else{
     setMode('light');
     document.body.style.backgroundColor = 'white';
+    showAlert("Light mode has been enabled", "success");
   }
 }
 
@@ -23,6 +37,7 @@ const toggleMode = ()=>{
       {/* <Navbar title="Textutils5" aboutText="About TextUtils"/> */}
       <Navbar title="Textutils" mode={mode} toggleMode={toggleMode}/>
       {/* <Navbar /> */}
+      <Alert alert={alert}/>
 
     <div className="container my-3">
       <TextForm heading="Enter the text to analyse" mode={mode}/>
